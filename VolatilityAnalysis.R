76916d3c2822d4da34bf5bc55dc9011fdeed92dc
Nikilesh Kumar <kumar.ni@husky.neu.edu>
2017-12-10 19:53:27 -0500

    Added Volatility Index Measure

--- VolatilityAnalysis.R
+++ VolatilityAnalysis.R
@@ -38,13 +38,13 @@ bb28 = BBands(mainDataset[c('Close')], n=28, sd = 2.0)
 dataWithBB = data.frame(mainDataset,bb28)
 head(dataWithBB, n=30)
 #Plot graph for closing prices to act as basis. Divide by 4 hack to display days 
-plot(mainDataset$DayCount/4,mainDataset$Close,xlab = "Days Elapsed", ylab = "Price", type = "l")
+plot(mainDataset$Timestamp,mainDataset$Close,xlab = "Days Elapsed", ylab = "Price", type = "l")
 title("Base Graph With Price History", sub = NULL)
 #Graph on log scale for Price
-plot(mainDataset$DayCount/4,mainDataset$Close,xlab = "Days Elapsed", ylab = "Price (Log)", type = "l", log ="y")
+plot(mainDataset$Timestamp/4,mainDataset$Close,xlab = "Days Elapsed", ylab = "Price (Log)", type = "l", log ="y")
 title("Base Graph With Price History (Log)", sub = NULL)
 #Plot data into graph1 for first 6 months
-plot(mainDataset$DayCount,mainDataset$Close,xlab = "6 Hour Blocks Elapsed", ylab = "Price", type = "l")
+plot(mainDataset$Timestamp,mainDataset$Close,xlab = "6 Hour Blocks Elapsed", ylab = "Price", type = "l")
 title("Bollinger Band Graph 1",sub=NULL)
 lines(dataWithBB$Close, col='blue')
 lines(dataWithBB$up, col='green')
@@ -56,9 +56,9 @@ lines(dataWithBB$mavg, col='purple')
 #
 #
 #Volatiltiy As A Factor of Range from upper and lower Bollinger Bands
-plot(dataWithBB$DayCount/4,dataWithBB$pctB,xlab = "Days Elapsed", ylab = "Percentage Change", type="l")
+plot(dataWithBB$Timestamp/4,dataWithBB$pctB,xlab = "Days Elapsed", ylab = "Percentage Change", type="l")
 #Outlier Analysis by smoothing using a log function
-plot(dataWithBB$DayCount/4,dataWithBB$pctB,xlab = "Days Elapsed", ylab = "Log of Percent Change", type="l", log = "y")
+plot(dataWithBB$Timestamp/4,dataWithBB$pctB,xlab = "Days Elapsed", ylab = "Log of Percent Change", type="l", log = "y")
 #Clearly we are missing out on data lesser than 0, hence, we construct a volatility index from absolute values of percent change
 Vindex = data.frame(dataWithBB$pctB)
 #Change name of column from pctB to Percent Change
@@ -66,8 +66,9 @@ names(Vindex) <- c("PercentChange")
 Vindex$PercentChange <- abs((Vindex$PercentChange*100-100))
 allData = data.frame(dataWithBB,Vindex)
 #Plot graph of Volatility Index
-plot(dataWithBB$DayCount/4, Vindex$PercentChange, xlab = "Days Elapsed", ylab = "Absolute value (log Index) ", type="l", log = "y")
+plot(dataWithBB$Timestamp/4, Vindex$PercentChange, xlab = "Days Elapsed", ylab = "Absolute value (log Index) ", type="l", log = "y")
 #Write back to original table
 write.table(allData,file="6HrDayCount.csv",na="",sep =",",row.names=FALSE)
-
+Vindex2 = Vindex$PercentChange[complete.cases(Vindex$PercentChange)]
+plot(allData$Timestamp[complete.cases(allData$PercentChange)]/4, 1/(0.75*Vindex2), xlab = "Number of Days", ylab = "Volatility Magnitude", type="l", col="blue")
 
